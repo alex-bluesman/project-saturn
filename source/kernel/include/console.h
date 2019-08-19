927bec823848bed680ef05d2ec1f054a0a2f9690
Alexander Smirnov <alex.bluesman.smirnov@gmail.com>
2019-08-19 23:53:02 +0300

    console: Add wrapper for debug output
    Wrap standard console output with debug macro. All debug output is enabled
    only when DEBUG symbol is defined.
    
    Signed-off-by: Alexander Smirnov <alex.bluesman.smirnov@gmail.com>

--- source/kernel/include/console.h
+++ source/kernel/include/console.h
@@ -12,4 +12,12 @@ void console_init(void);
 void uart_init(void);
 void uart_tx(uint8_t *buff, size_t len);
 
+#ifdef DEBUG
+  #define console_dbg(msg, ...)		console_msg("(debug) "msg)
+  #define console_dbg_va(msg, ...)	console_msg("(debug) "msg, __VA_ARGS__)
+#else
+  #define console_dbg(msg, ...)
+  #define console_dbg_va(msg, ...)
+#endif /* DEBUG */
+
 #endif /* __CONSOLE_H__ */
