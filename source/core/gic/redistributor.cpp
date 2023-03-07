2bd53d0898a2ca9ef6b05087805f1088e07cbebc
Alexander Smirnov <alex.bluesman.smirnov@gmail.com>
2023-03-07 17:42:02 +0100

    GICv3: Basic initialization for primary core
    This patch introduces initialization of:
     - Distributor
     - Redistributor
     - CPU interface
    
    The GIC is configured to route all the interrupts to the primary core in
    the cluster. For testing purpose, after the initialization we send an
    SGI for ourselfs, what leads to respective console message and switch to
    the fault mode.
    
    Signed-off-by: Alexander Smirnov <alex.bluesman.smirnov@gmail.com>

---
+++ source/core/gic/redistributor.cpp
@@ -0,0 +1,64 @@
+#include "redistributor.hpp"
+
+#include <arm64/registers>
+#include <iconsole>
+#include <platform/qemuarm64>
+
+namespace saturn {
+namespace core {
+
+enum Gic_Redist
+{
+	CTRL		= 0x0000,
+	WAKER		= 0x0014,
+	IGROUPR0	= 0x0080,
+	ISENABLER0	= 0x0100,
+	ICENABLER0	= 0x0180,
+	ICACTIVER0	= 0x0380,
+	IPRIORITYR	= 0x0400,
+	SGI_offset	= 0x10000	// 64K SGI region offset
+};
+
+GicRedistributor::GicRedistributor()
+{
+	// TBD: check return value
+	Regs = new MMap(MMap::IO_Region(_gic_redist_addr));
+
+	// Wake up redistributor
+	uint32_t reg = Regs->Read<uint32_t>(Gic_Redist::WAKER);
+	reg &= ~(1 << 1); // Clear ProcessorSleep flag
+	Regs->Write<uint32_t>(Gic_Redist::WAKER, reg);
+
+	// Wait until redistributor is available
+	// TBD: should we introduce timeout?
+	while (Regs->Read<uint32_t>(Gic_Redist::WAKER) & (1 << 2));
+
+	for (size_t i = 0; i < 16; i += 4)
+	{
+		Regs->Write<uint32_t>(SGI_offset + Gic_Redist::IPRIORITYR + (i / 4) * 4, 0x40404040);
+	}
+
+	for (size_t i = 16; i < 32; i += 4)
+	{
+		Regs->Write<uint32_t>(SGI_offset + Gic_Redist::IPRIORITYR + (i / 4) * 4, 0x80808080);
+	}
+
+	// Disable all the SGIs and PPIs on start up
+	Regs->Write<uint32_t>(SGI_offset + Gic_Redist::ICACTIVER0, 0xffffffff);
+
+	// Disable all the SGIs and PPIs on start up
+	Regs->Write<uint32_t>(SGI_offset + Gic_Redist::ICENABLER0, 0xffff0000);
+	Regs->Write<uint32_t>(SGI_offset + Gic_Redist::ISENABLER0, 0x0000ffff);
+
+	// Group 1
+	Regs->Write<uint32_t>(SGI_offset + Gic_Redist::IGROUPR0, 0xffffffff);
+
+	// Wait until write operations are done
+	// TBD: should we introduce timeout?
+	while (Regs->Read<uint32_t>(Gic_Redist::CTRL) & (1 << 31));
+
+	Log() << "  //redistributor initialized" << fmt::endl;
+}
+
+}; // namespace core
+}; // namespace saturn
--- source/core/interrupts.cpp
+++ source/core/interrupts.cpp
@@ -1,22 +1,65 @@
+#include "gic/cpu_interface.hpp"
 #include "gic/distributor.hpp"
+#include "gic/redistributor.hpp"
+
 #include <fault>
 
 namespace saturn {
 namespace core {
 
+static CpuInterface* CpuIface = nullptr;
 static GicDistributor* GicDist = nullptr;
+static GicRedistributor* GicRedist = nullptr;
+
+static inline void Local_IRQ_Enable()
+{
+	asm volatile (
+			"msr daifclr, #2\n"
+			"isb\n"
+			::: "memory"
+		     );
+}
 
 void Interrupts_Init()
 {
+	// GICv3 consists from the following logical components:
+	//
+	//  --- Interrupt Routing Infrastructure-----   --- CPU Cluster ----
+	// |   -------------       ---------------   | |   ---------------  |
+	// |  | Distributor | <-> | Redistributor | <---> | CPU Interface | |
+	// |   -------------       ---------------   | |   ---------------  |
+	//  -----------------------------------------   --------------------
+	//
+	// So let's construct objects for the complete routing chain
+
 	// TBD: we should distinguish primary CPU and secondary.
 	//      GIC dist should be initialized only for primary CPU.
 	Log() << "create interrupts infrastructure" << fmt::endl;
+
 
 	GicDist = new GicDistributor();
 	if (nullptr == GicDist)
 	{
 		Fault("GIC distributor allocation failed");
 	}
+
+	GicRedist = new GicRedistributor();
+	if (nullptr == GicRedist)
+	{
+		Fault("GIC redistributor allocation failed");
+	}
+
+	CpuIface = new CpuInterface();
+	if (nullptr == CpuIface)
+	{
+		Fault("GIC CPU interface allocation failed");
+	}
+
+	// Now we are ready to ebable IRQs
+	Local_IRQ_Enable();
+
+	// Debug purpose:
+	GicDist->SendSGI();
 }
 
 }; // namespace core
