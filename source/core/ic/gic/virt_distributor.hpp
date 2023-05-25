7b83f3bdb9a7e15e9cf411824126ffd6ba56a25d
Alexander Smirnov <alex.bluesman.smirnov@gmail.com>
2023-05-25 14:38:45 +0200

    vgicd: Add virtual GIC distributor
    This patch introduces basic implementation for GIC distributor. For now
    it just store and provide values to VMs without any processing logic.
    
    Signed-off-by: Alexander Smirnov <alex.bluesman.smirnov@gmail.com>

---
+++ source/core/ic/gic/virt_distributor.hpp
@@ -0,0 +1,43 @@
+// Copyright (C) 2023 Alexander Smirnov <alex.bluesman.smirnov@gmail.com>
+//
+// Licensed under the MIT License (the "License"); you may not use this file except
+// in compliance with the License. You may obtain a copy of the License at
+//
+// http://opensource.org/licenses/MIT
+//
+// Unless required by applicable law or agreed to in writing, software distributed 
+// under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR 
+// CONDITIONS OF ANY KIND, either express or implied. See the License for the 
+// specific language governing permissions and limitations under the License.
+
+#pragma once
+
+#include "distributor.hpp"
+
+#include <mtrap>
+
+namespace saturn {
+namespace core {
+
+// Forward declaration:
+class GicDistributor;
+
+class VirtGicDistributor : public IVirtIO
+{
+public:
+	VirtGicDistributor(GicDistributor&);
+	~VirtGicDistributor();
+
+public:
+	void Read(uint64_t addr, void* data, AccessSize size);
+	void Write(uint64_t addr, void* data, AccessSize size);
+	bool IRq_Enabled(uint32_t nr);
+
+private:
+	MTrap* mTrap;
+	GicDistributor& gicDist;
+	GicDistRegs& vGicState;
+};
+
+}; // namespace core
+}; // namespace saturn
